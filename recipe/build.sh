721edca1e93f8d0bd4afe0300372c5677b46df86
Isuru Fernando <isuruf@gmail.com>
2025-01-22 15:35:03 +0530

    cleanup, win support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,14 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-./configure --disable-dependency-tracking --prefix=${PREFIX}
-make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+./configure \
+  --disable-dependency-tracking \
+  --prefix=${PREFIX} \
+  --enable-shared \
+  --disable-static \
+
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  make check
 fi
 make install
